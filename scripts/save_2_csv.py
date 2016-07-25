2b912ce54dda5117f24acb7b959a9cb89464802e
sheltowt <sheltowt@gmail.com>
2016-07-25 15:26:29 -0400

    save to csv

--- scripts/save_2_csv.py
+++ scripts/save_2_csv.py
@@ -8,12 +8,22 @@ from pymongo import MongoClient
 
 config = load_config()
 
-print(config)
-
 client = MongoClient(config["database"]["connection_url"])
 
 db = client[config["database"]["database_name"]]
 conn = db[config["database"]["collection_name"]]
+
+data = conn.find()
+
+
+with open("../tmp/tweets.csv", 'w') as csvfile:
+	writer = csv.DictWriter(csvfile, fieldnames=["text"])
+	writer.writeheader()
+	for document in data:
+		sample = {}
+		sample["text"] = document["text"]
+		writer.writerow(sample)
+
 
 
 

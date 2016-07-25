70b18dcf0684fddbdb730b51d9879a845583d669
sheltowt <sheltowt@gmail.com>
2016-07-25 15:14:04 -0400

    adding boiler plate code

--- scripts/save_2_csv.py
+++ scripts/save_2_csv.py
@@ -1,3 +1,19 @@
 import csv
+import pymongo
+import sys
+import json
+sys.path.append("../")
+from import_config import load_config
+from pymongo import MongoClient
+
+config = load_config()
+
+print(config)
+
+client = MongoClient(config["database"]["connection_url"])
+
+db = client[config["database"]["database_name"]]
+conn = db[config["database"]["collection_name"]]
+
 
 

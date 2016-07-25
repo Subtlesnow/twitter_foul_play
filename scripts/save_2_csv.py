4f291aecf10411730ac6c7363d97adac8cc2f17e
sheltowt <sheltowt@gmail.com>
2016-07-25 15:33:51 -0400

    adding update to resolve steeves comment

--- scripts/save_2_csv.py
+++ scripts/save_2_csv.py
@@ -8,13 +8,12 @@ from pymongo import MongoClient
 
 config = load_config()
 
-client = MongoClient(config["database"]["connection_url"])
+client_bla = MongoClient(config["database"]["connection_url"])
 
-db = client[config["database"]["database_name"]]
+db = client_bla[config["database"]["database_name"]]
 conn = db[config["database"]["collection_name"]]
 
 data = conn.find()
-
 
 with open("../tmp/tweets.csv", 'w') as csvfile:
 	writer = csv.DictWriter(csvfile, fieldnames=["text"])

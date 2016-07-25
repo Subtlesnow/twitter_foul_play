f48ee81bffd85e59c4d84e18904ea755cd72753c
Steven VanWoerkom <stevenvanwoerkom@Stevens-MacBook-Pro.local>
2016-07-25 17:46:11 -0400

    testing

--- scripts/natural_lang.py
+++ scripts/natural_lang.py
@@ -8,18 +8,19 @@ from pymongo import MongoClient
 import nltk
 
 config = load_config()
-
+print(config)
 client = MongoClient(config["database"]["connection_url"])
-
+print(client)
 db = client[config['database']["database_name"]]
-con = db[config["database"]["collection_name"]]
-
+print(db)
+conn = db[config["database"]["collection_name"]]
+print(conn)
 data = conn.find()
+print(data)	
 for twitter_object in data:
 	twitter_object["process_text"] = nltk.word_tokenize(twitter_object['text'])
-	twitter_object["parts_of_speech"] = nltk.pos_tag(twitter_object["process_text"])
+	twitter_object["natural_lang"] = nltk.pos_tag(twitter_object["process_text"])
 	conn.save(twitter_object)
-
 
 
 
